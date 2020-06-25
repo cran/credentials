86e5b913df6d903a281a1e0e0ea25452c5259048
Jeroen Ooms <jeroen@berkeley.edu>
2020-06-25 13:30:07 +0000

    version 1.2.0

--- R/github-pat.R
+++ R/github-pat.R
@@ -2,15 +2,21 @@
 #'
 #' Populates the `GITHUB_PAT` environment variable using the [git_credential][http_credentials]
 #' manager, which `git` itself uses for storing passwords. The credential manager
-#' will automatically and securely prompt the user for credentials when needed.
+#' returns stored credentials if available, and securely prompt the user for
+#' credentials when needed.
 #'
-#' Use this function in your [.Rprofile][Startup] script to automatically set
-#' `GITHUB_PAT` for each R session without hardcoding your any secrets in plain text.
+#' Packages that require a `GITHUB_PAT` can call this function to automatically
+#' set the `GITHUB_PAT` when needed. Users may call this function in their
+#' [.Rprofile][Startup] script to automatically set `GITHUB_PAT` for each R
+#' session without hardcoding any tokens on disk in plain-text.
 #'
 #' @export
-#' @param force_new forget existing pat, always ask for new one
-#' @param verbose print some debugging messages
-set_github_pat <- function(force_new = FALSE, verbose = TRUE){
+#' @param force_new forget existing pat, always ask for new one.
+#' @param validate checks with the github API that this token works. Defaults to
+#' `TRUE` only in an interactive R session (not when running e.g. CMD check).
+#' @param verbose prints a message showing the credential helper and PAT owner.
+#' @return Returns `TRUE` if a valid GITHUB_PAT was set, and FALSE if not.
+set_github_pat <- function(force_new = FALSE, validate = interactive(), verbose = validate){
   if(isTRUE(force_new))
     git_credential_forget('https://token@github.com')
   if(isTRUE(verbose))
@@ -24,28 +30,35 @@ set_github_pat <- function(force_new = FALSE, verbose = TRUE){
     on.exit(Sys.unsetenv('GIT_ASKTOKEN'), add = TRUE)
   }
   for(i in 1:3){
-    cred <- git_credential_ask('https://token@github.com', verbose = verbose)
+    # The username doesn't have to be real, Github seems to ignore username for PATs
+    pat_user <- Sys.getenv("GITHUB_PAT_USER", 'token')
+    cred <- git_credential_ask(sprintf('https://%s@github.com', pat_user), verbose = verbose)
     if(length(cred$password)){
       if(nchar(cred$password) < 40){
         message("Please enter a token in the password field, not your master password! Let's try again :-)")
         message("To generate a new token, visit: https://github.com/settings/tokens")
         credential_reject(cred)
-      } else {
+        next
+      }
+      if(isTRUE(validate)) {
         hx <- curl::handle_setheaders(curl::new_handle(), Authorization = paste("token", cred$password))
         req <- curl::curl_fetch_memory("https://api.github.com/user", handle = hx)
-        if(req$status >= 400){
+        if(req$status_code >= 400){
           message("Authentication failed. Token invalid.")
           credential_reject(cred)
-        } else {
+          next
+        }
+        if(verbose == TRUE){
           data <- jsonlite::fromJSON(rawToChar(req$content))
-          if(verbose == TRUE){
-            Sys.setenv(GITHUB_PAT = cred$password)
-            helper <- tryCatch(credential_helper_get()[1], error = "??")
-            message(sprintf("Using GITHUB_PAT from %s (credential helper: %s)", data$name, helper))
-            return(invisible())
-          }
+          helper <- tryCatch(credential_helper_get()[1], error = function(e){"??"})
+          message(sprintf("Using GITHUB_PAT from %s (credential helper: %s)", data$name, helper))
         }
       }
+      return(Sys.setenv(GITHUB_PAT = cred$password))
     }
   }
+  if(verbose == TRUE){
+    message("Failed to find a valid GITHUB_PAT after 3 attempts")
+  }
+  return(FALSE)
 }
--- inst/doc/intro.R
+++ inst/doc/intro.R
@@ -1,4 +1,4 @@
-## ---- echo=FALSE---------------------------------------------------------
+## ---- echo=FALSE--------------------------------------------------------------
 has_git <- credentials:::has_git_cmd()
 delete_git_config_on_exit <- !file.exists('~/.gitconfig')
 credentials:::set_default_cred_helper()
@@ -11,27 +11,27 @@ library <- function(package){
                       })
 }  
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 library(credentials)
 
-## ---- eval=has_git-------------------------------------------------------
+## ---- eval=has_git------------------------------------------------------------
 credential_helper_get()
 
-## ---- echo=FALSE, eval=has_git-------------------------------------------
+## ---- echo=FALSE, eval=has_git------------------------------------------------
 # This hack may not work on MacOS server where cred helper is osxkeychain 
 # which always requires user interaction. Hence error=TRUE in the next block.
 example <- list(protocol = "https", host = "example.com",
   username = "jeroen", password = "supersecret")
 credential_approve(example)
 
-## ----error=TRUE, eval=has_git--------------------------------------------
+## ----error=TRUE, eval=has_git-------------------------------------------------
 library(credentials)
 git_credential_ask('https://example.com')
 
-## ---- echo=FALSE, eval=has_git-------------------------------------------
+## ---- echo=FALSE, eval=has_git------------------------------------------------
 credential_reject(list(protocol = "https", host = "example.com"))
 
-## ---- echo = FALSE-------------------------------------------------------
+## ---- echo = FALSE------------------------------------------------------------
 ssh_key_info <- function(){
   try({
     out <- credentials:::ssh_key_info(auto_keygen = FALSE)
@@ -40,10 +40,10 @@ ssh_key_info <- function(){
   })
 }
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ssh_key_info()
 
-## ----echo=FALSE----------------------------------------------------------
+## ----echo=FALSE---------------------------------------------------------------
 if(isTRUE(delete_git_config_on_exit))
   unlink("~/.gitconfig")
 
